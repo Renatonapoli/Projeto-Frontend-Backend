d77dd65917220201132fcc5c0fa627ae5ec9744f
Renato Napoli <renato.napoli@nagro.app>
2022-02-16 12:58:21 -0300

    Projeto Fontend/Backend

---
+++ frontend/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<Header />
+		<Menu />
+		<Content />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from "@/components/template/Header.vue";
+import Menu from "@/components/template/Menu.vue";
+import Content from "@/components/template/Content.vue";
+import Footer from "@/components/template/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Menu,
+    Content,
+    Footer,
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: "Lato", "sans-serif";
+}
+body {
+  margin: 0;
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 60px 1fr 40px;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
+</style>
---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Footer">
+    <span>Copyright <strong>Renato Napoli</strong>&copy 2018-2022</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+.footer {
+  grid-area: footer;
+  background-color: #ddd;
+  color: #333;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 10px;
+}
+</style>
