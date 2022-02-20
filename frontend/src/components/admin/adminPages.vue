27568b485346068b37b931822d29dcccb81a1b5d
Renato Napoli <renato.napoli@nagro.app>
2022-02-20 16:29:15 -0300

    Feat: Componente Admin

---
+++ frontend/src/components/admin/adminPages.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="admin">
+    <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
+      sub="Cadastros & Cia" />
+  </div>
+</template>
+
+<script>
+import PageTitle from "../admin/PageTitle";
+
+export default {
+  name: "AdiminPages",
+  components: { PageTitle },
+};
+</script>
+
+<style>
+</style>
